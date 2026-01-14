12d26d10dac01b1cd367df4f0546992ee9545db5
Nahuel Garcia <nhlgrc@hotmail.com>
2026-01-14 14:02:15 -0300

    sadsad

--- lib/presentation/screens/discover/discover_screen.dart
+++ lib/presentation/screens/discover/discover_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+import 'package:toktik/presentation/widgets/shared/video_scrollable_view.dart';
 
 class DiscoverScreen extends StatelessWidget {
   
@@ -6,10 +9,14 @@ class DiscoverScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    //final discoverProvider = Provider.of<DiscoverProvider>(context);
+    final discoverProvider = context.watch<DiscoverProvider>();
+
     return Scaffold(
-      body: Center(
-        child: Text('DiscoverScreen'),
-      ),
+      body: discoverProvider.initialLoading
+        ? const Center( child: CircularProgressIndicator(strokeWidth: 2,) )
+        : VideoScrollableView(videos: discoverProvider.videos)
     );
   }
 }
